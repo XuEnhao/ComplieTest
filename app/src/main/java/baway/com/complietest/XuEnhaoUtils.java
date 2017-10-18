aa7337d94e8d60da1fd75277608e9ceecad7bbac
XuEnhao <569283950@qq.com>
2017-10-18 15:45:15 +0800

    xuenhaoUtils

---
+++ app/src/main/java/baway/com/complietest/XuEnhaoUtils.java
@@ -0,0 +1,13 @@
+package baway.com.complietest;
+
+import android.util.Log;
+
+/**
+ * Created by 56928 on 2017/10/18.
+ */
+
+public class XuEnhaoUtils {
+    static void show(){
+        Log.d("Utils", "show: ");
+    }
+}
